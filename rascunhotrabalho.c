313a1ba746d29a4f531263dc1ac3c1c8ab406713
Marcelly Almeida <marcellyalmeida890@gmail.com>
2025-11-03 17:55:24 -0400

    inicio

---
+++ rascunhotrabalho.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("Escolha uma opcao:\n");
+    printf("1- Areas Comuns\n2- Solicitar Reserva\n3- Relatorio\n");
+
+    return 0;
+}
