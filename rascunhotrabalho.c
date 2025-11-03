4be174b4168c98391b3976ead1ca224729d048f5
Marcelly Almeida <marcellyalmeida890@gmail.com>
2025-11-03 18:00:42 -0400

    Update rascunhotrabalho.c

--- rascunhotrabalho.c
+++ rascunhotrabalho.c
@@ -3,8 +3,10 @@
 
 int main()
 {
+    int opcao;
     printf("Escolha uma opcao:\n");
-    printf("1- Areas Comuns\n2- Solicitar Reserva\n3- Relatorio\nmaisumteste");
+    printf("1- Areas Comuns\n2- Solicitar Reserva\n3- Relatorio\n4- Sair\n");
+    scanf("%d", &opcao); 
 
     return 0;
 }
